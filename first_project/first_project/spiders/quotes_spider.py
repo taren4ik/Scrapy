6fa79a46afa434f28738145a7ec573e8c5149f06
Dmitry Permyakov <taren4ik@mail.ru>
2023-11-22 17:42:27 +1000

    Создан паук по сбору ключевой информации по квартирам на сайте

--- first_project/first_project/spiders/quotes_spider.py
+++ first_project/first_project/spiders/quotes_spider.py
@@ -1,36 +1,47 @@
 from pathlib import Path
-
+import time
 import scrapy
 
+from bs4 import BeautifulSoup
 
-class QuotesSpider(scrapy.Spider):
-    name = 'farpost_flat'
 
-    def start_requests(self):
-        urls = [
-            "https://www.farpost.ru/vladivostok/realty/sell_flats/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+class Spyder(scrapy.Spider):
+    name = 'farpost_spider'
+    start_urls = ['https://www.farpost.ru/vladivostok/realty/sell_flats/']
 
     def parse(self, response):
-        for link in response.css('div.bull-item-content__subject-container a::attr(href)').get()
-            yield  response.follow(link, callback=self.parse_flat)
-        for i in range (1,10):
-            next_page = (
-            f'https://www.farpost.ru/vladivostok/realty/sell_flats?page={i}')
-        yield response.follow(next_page, callback=self.parse)
+        """
+        Осуществляет переход по страницам и объявлениям внутри страницы.
+        :param response:
+        :return:
+        """
+        for link in response.css(
+                'div.bull-item-content__subject-container a::attr(href)'):
+            time.sleep(1)
+            yield response.follow(link, callback=self.parse_flat)
 
+        for i in range(1, 10):  # переход по страницам
+            next_page = (f'https://www.farpost.ru/vladivostok/realty/sell_flats?page={i}')
+            yield response.follow(next_page, callback=self.parse)
 
     def parse_flat(self, response):
+        """
+        Сбор информации по каждой квартире.
+        :param response:
+        :return:
+        """
+        soup = BeautifulSoup(response.text, "html.parser")
         yield {
-
-            'buy': response.css('div.viewbull-summary-price__realty-price '
-                                'span::attr(data-bulletin-price)').get(),
-            'address': response.css('span.auto-shy::text').get()
+              'srteet_district': soup.find(
+                  'span', {'data-field': 'street-district'}).text.strip(),
+              'title': soup.find(
+                  'span', {'data-field': 'subject'}).text.strip(),
+              'material': soup.find(
+                  'span', {'data-field': 'wallMaterial'}).text.strip(),
+              'area': soup.find(
+                  'span', {'data-field': 'areaTotal-share'}).text.strip(),
+              'count_rooms': soup.find(
+                  'span', {'data-field': 'flatType'}).text.strip(),
+              'price': soup.find(
+                  'span', class_="viewbull-summary-price__value").text.strip()
         }
-          #  'name':
-          #  'district':
-          #  'type':
-          # 'square':
-
