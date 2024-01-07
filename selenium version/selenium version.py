89dd44642fecc9f2b19c5f7a710fe8cb94c272cd
Dmitry Permyakov <taren4ik@mail.ru>
2024-01-07 23:54:12 +1000

    Добавил сохранение сессии для webdriver.

--- selenium version/selenium version.py
+++ selenium version/selenium version.py
@@ -1,4 +1,3 @@
-import requests
 import random
 from bs4 import BeautifulSoup
 import csv
@@ -8,76 +7,6 @@ import numpy as np
 import time
 import datetime
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-
-# def extract_profile_info(profile_html):
-#     soup = BeautifulSoup(profile_html, "html.parser")
-#     flat_data = {}
-#
-#     if soup.find(
-#             'span', {'data-field': 'street-district'}).text.strip():
-#         flat_data['srteet_district'] = soup.find(
-#             'span', {'data-field': 'street-district'}).text.strip()
-#     else:
-#         flat_data['srteet_district'] = None
-#     if soup.find(
-#             'span', {'data-field': 'subject'}).text.strip():
-#
-#         flat_data['title'] = soup.find(
-#             'span', {'data-field': 'subject'}).text.strip()
-#     else:
-#         flat_data['title'] = None
-#     if soup.find(
-#             'span', {'data-field': 'wallMaterial'}).text.strip():
-#         flat_data['material'] = soup.find(
-#             'span', {'data-field': 'wallMaterial'}).text.strip()
-#     else:
-#         flat_data['material'] = None
-#
-#     if soup.find(
-#             'span', {'data-field': 'areaTotal-share'}).text.strip()[:-7:]:
-#
-#         flat_data['area'] = soup.find(
-#             'span', {'data-field': 'areaTotal-share'}).text.strip()[:-7:]
-#     else:
-#         flat_data['area'] = None
-#
-#     if soup.find(
-#             'span', {'data-field': 'flatType'}).text.strip():
-#
-#         flat_data['count_rooms'] = soup.find(
-#             'span', {'data-field': 'flatType'}).text.strip()
-#     else:
-#         flat_data['count_rooms'] = None
-#
-#     if soup.find('span',
-#                  class_="viewbull-summary-price__value").text.strip()[
-#        :-1:].replace(' ', ''):
-#         flat_data['price'] = soup.find('span',
-#                   class_="viewbull-summary-price__value").text.strip()[
-#                              :-1:].replace(' ', '')
-#     else:
-#
-#         flat_data['price'] = None
-#     return flat_data
-
-
-# def safe_request(url, headers):
-#     """
-#     Выполняет безопасный HTTP-запрос с повторными попытками
-#     :param url:
-#     :param headers:
-#     :return:
-#     """
-#     while True:
-#         try:
-#             response = requests.get(url, headers=headers)
-#             response.raise_for_status()  # проверяет, что ответ успешен
-#             return response
-#         except requests.RequestException as e:
-#             print(f"Error fetching {url}: {e}. Retrying in 7 seconds...")
-#             time.sleep(7)
 
 
 def write_profiles_to_csv(df):
@@ -89,18 +18,9 @@ def write_profiles_to_csv(df):
     filename = f"profiles_farpost_{datetime.date.today().__str__().replace('-','_')}.csv"
     df.to_csv(f'{filename}', mode='a',
                    sep=';',
-                   header=True,
+                   header=False,
                    index=False,
                    encoding='utf-16')
-    # fieldnames = set()
-    # for profile in profiles:
-    #     fieldnames.update(profile.keys())
-    #
-    # with open(filename, 'a', newline='', encoding='utf-8') as file:
-    #     writer = csv.DictWriter(file, fieldnames=fieldnames)
-    #     writer.writeheader()
-    #     for profile in profiles:
-    #         writer.writerow(profile)
 
 
 def scrape_all_profiles(start_url):
@@ -113,6 +33,8 @@ def scrape_all_profiles(start_url):
     square = []
     is_check = []
     room = []
+    views = []
+    post_id = []
     current_url = start_url
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument(
@@ -122,6 +44,10 @@ def scrape_all_profiles(start_url):
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-browser-side-navigation')
     chrome_options.add_argument('--disable-gpu')
+ #   chrome_options.add_argument('--allow-profiles-outside-user-dir')
+ #   chrome_options.add_argument('--enable-profile-shortcut-manager')
+    chrome_options.add_argument(r'user-data-dir=D:\developer\scrapy')
+    chrome_options.add_argument('--profile-directory=Profile 1')
     user_agents = [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Mozilla/5.0 (Linux; Android 9; SM-T385) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36',
@@ -142,15 +68,14 @@ def scrape_all_profiles(start_url):
     while current_url:
         if current_url == 'https://www.farpost.ru/vladivostok/realty/sell_flats/':
             driver = webdriver.Chrome(options=chrome_options)
-            driver.implicitly_wait(7)
-            driver.get(current_url)
         else:
             driver.execute_script("window.open('', '_blank');")
 
             # Переключение на новую вкладку (по индексу, где 1 - вторая вкладка)
             driver.switch_to.window(driver.window_handles[1])
-            driver.get(current_url)
-        time.sleep(random.uniform(3, 6))
+        driver.implicitly_wait(10)
+        driver.get(current_url)
+        time.sleep(random.uniform(5, 10))
         response = driver.page_source
         soup = BeautifulSoup(response, 'html.parser')
 
@@ -162,19 +87,24 @@ def scrape_all_profiles(start_url):
         full_post = full_post_v1 if full_post_v1 else full_post_v2
 
         for post in full_post:
+            post_id.append(post.find('a')['name'])
             if post.find('div',
                          class_='bull-item-content__price-info-container').text:
                 is_check.append('True')
             else:
                 is_check.append('False')
 
+            if post.find('span',  class_='views nano-eye-text'):
+                views.append(
+                    post.find('span',  class_='views nano-eye-text').text)
+            else:
+                views.append('0')
+
         profile_links = [a["href"] for a in soup.find_all("a",
                                                           class_="bulletinLink bull-item__self-link auto-shy")]
         name_announcement = [a.text for a in soup.find_all("a",
                                                            class_="bulletinLink bull-item__self-link auto-shy")]
         for value in name_announcement:
-            #is_check.append('False' if ('ЖК' in value) else 'True')
-
             room.append(value[0] if value[0].isdigit() or value.startswith(
                 'Гостинка') or value.startswith('Комната') else 0)
 
@@ -187,16 +117,15 @@ def scrape_all_profiles(start_url):
         for value in district:
             if value.split(',')[0] == '64':
                 area.append('64,' + value.split(',')[1])
+                author.append(value.split(',')[2])
             else:
                 area.append(value.split(',')[0])
+                author.append(value.split(',')[1])
             square.append(value.split(',')[-2] + ',' + value.split(',')[-1][
                 0] if len(value.split(',')) > 2 else 0)
-            author.append(value.split(',')[1] if value.split(',')[1] else
-                           'None')
-
-        views = [span.text for span in soup.find_all("span", class_="views nano-eye-text")]
 
         df = pd.DataFrame({
+            'Id': post_id,
             'Название': name_announcement,
             'Район': 'None',
             'Ссылка': profile_links,
@@ -204,13 +133,16 @@ def scrape_all_profiles(start_url):
             'Стоимость': 'None',
             'Комнат': room,
             'Формат': is_check,
-            'Площадь': 'None'
+            'Площадь': 'None',
+            'Автор': 'None',
+
 
         })
         for i, row in enumerate(np.where(df['Формат'] == 'True')[0].tolist()):
             df.loc[row, 'Стоимость'] = cost[i]
             df.loc[row, 'Район'] = area[i]
             df.loc[row, 'Площадь'] = square[i]
+            df.loc[row, 'Автор'] = author[i]
 
         write_profiles_to_csv(df)
         df = df[0:0]
@@ -219,6 +151,8 @@ def scrape_all_profiles(start_url):
         square = []
         area = []
         room = []
+        views = []
+        post_id = []
         if page > 1:
             driver.close()
             driver.switch_to.window(driver.window_handles[0])
@@ -226,12 +160,12 @@ def scrape_all_profiles(start_url):
         current_url = (
             f'https://www.farpost.ru/vladivostok/realty/sell_flats?page={page}')
 
-        time.sleep(random.uniform(2, 5))
+        time.sleep(random.uniform(2, 7))
     driver.switch_to.window(driver.window_handles[0])
     driver.quit()
     return True
 
 
-# Начните с извлечения всех объявлений
+
 all_profiles = scrape_all_profiles(
     "https://www.farpost.ru/vladivostok/realty/sell_flats/")
