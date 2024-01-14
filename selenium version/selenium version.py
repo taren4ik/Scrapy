4c7ec93f89c6b77d282be150ccd1faa205f1b4c2
Dmitry Permyakov <taren4ik@mail.ru>
2024-01-15 00:57:04 +1000

    Change for pep8 + user_agents.py

--- selenium version/selenium version.py
+++ selenium version/selenium version.py
@@ -1,16 +1,16 @@
-import os
 import datetime
+import os
 import random
 import time
 
-import psycopg2
-from sqlalchemy import create_engine
-from bs4 import BeautifulSoup
-import pandas as pd
-from selenium import webdriver
 import numpy as np
+import pandas as pd
+import psycopg2
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-
+from selenium import webdriver
+from sqlalchemy import create_engine
+from user_agents import USER_AGENTS
 
 load_dotenv()
 
@@ -21,6 +21,7 @@ def timer_wrapper(func):
     :param func:
     :return:
     """
+
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
@@ -28,19 +29,26 @@ def timer_wrapper(func):
         difference_time = end_time - start_time
         print(f"Функция {func.__name__} выполнилась за {difference_time:.4f} секунд.")
         return result
+
     return wrapper
 
 
 def write_profiles_db(df):
+    """
+    Запись данных в конечную структуру.
+    :param df:
+    :return:
+    """
     connection = psycopg2.connect(
-        host=os.getenv('DB_HOST'),
-        database=os.getenv('DB_NAME'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PORT')
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PORT"),
     )
     cur = connection.cursor()
 
-    cur.execute('''
+    cur.execute(
+        """
         CREATE TABLE farpost.farpost (
             id varchar NOT NULL,
             "text" varchar(512) NULL,
@@ -53,8 +61,8 @@ def write_profiles_db(df):
             square varchar(20) NULL,
             author varchar(20) NULL,
             "date" timestamp NULL DEFAULT now(),
-            CONSTRAINT farpost_pkey PRIMARY KEY (id)'''
-)
+            CONSTRAINT farpost_pkey PRIMARY KEY (id)"""
+    )
 
 
 def write_profiles_to_csv(df):
@@ -63,12 +71,11 @@ def write_profiles_to_csv(df):
     :param df:
     :return:
     """
-    filename = f"profiles_farpost_{datetime.date.today().__str__().replace('-','_')}.csv"
-    df.to_csv(f'{filename}', mode='a',
-                   sep=';',
-                   header=False,
-                   index=False,
-                   encoding='utf-16')
+    path = datetime.date.today().__str__().replace("-", "_")
+    filename = f"profiles_farpost_{path}.csv"
+    df.to_csv(
+        f"{filename}", mode="a", sep=";", header=False, index=False, encoding="utf-16"
+    )
 
 
 @timer_wrapper
@@ -86,119 +93,156 @@ def scrape_all_profiles(start_url):
     post_id = []
     current_url = start_url
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_argument('--disable-infobars')
-    chrome_options.add_argument('--disable-extensions')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--disable-browser-side-navigation')
-    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument(
+        "--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-browser-side-navigation")
+    chrome_options.add_argument("--disable-gpu")
     # chrome_options.add_argument('--allow-profiles-outside-user-dir')
     # chrome_options.add_argument('--enable-profile-shortcut-manager')
     # chrome_options.add_argument(r'user-data-dir=D:\developer\scrapy')
     # chrome_options.add_argument('--profile-directory=Profile 1')
-    user_agents = [
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Mozilla/5.0 (Linux; Android 9; SM-T385) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36',
-        'Mozilla/5.0 (Linux; Android 9; Redmi Note 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.127 Mobile Safari/537.36',
-        'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25',
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1',
-        'Mozilla/5.0 (Linux; Android 10; GM1910) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36',
-        'Mozilla/5.0 (Linux; Android 10; MI 8 Lite) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36',
-        'Mozilla/5.0 (Linux; Android 9; FLA-LX1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36 OPR/61.2.3076.56749',
-        'Mozilla/5.0 (Linux; arm_64; Android 7.1.1; Lenovo TB-8504X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 YaBrowser/20.6.1.73.01 Safari/537.36',
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/86.0.4240.93 Mobile/15E148 Safari/604.1',
-        'Mozilla/5.0 (Linux; Android 8.1.0; 16th) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.81 Mobile Safari/537.36',
-        'Mozilla/5.0 (Linux; Android 9) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 DuckDuckGo/5',
-    ]
+    user_agents = USER_AGENTS
 
     page = 1
     while current_url:
+        posts = []
         if page == 181:
             return True
         if page == 1 or page % 50 == 0:
-            chrome_options.add_argument(f'user-agent={random.choice(user_agents)}')
+            chrome_options.add_argument(
+                f"user-agent={random.choice(user_agents)}"
+            )
             driver = webdriver.Chrome(options=chrome_options)
         else:
             driver.execute_script("window.open('', '_blank');")
-
-            # Переключение на новую вкладку (по индексу, где 1 - вторая вкладка)
+        # Переключение на новую вкладку (по индексу, где 1 - вторая вкладка)
             driver.switch_to.window(driver.window_handles[1])
         driver.implicitly_wait(10)
         driver.get(current_url)
         time.sleep(random.uniform(3, 9))
         response = driver.page_source
-        soup = BeautifulSoup(response, 'html.parser')
+        soup = BeautifulSoup(response, "html.parser")
 
-        full_post_v1 = [div for div in soup.find_all("div",
-                                                   class_="descriptionCell bull-item-content__cell bull-item-content__description-cell")]
+        full_post_v1 = [
+            div
+            for div in soup.find_all(
+                "div",
+                class_="descriptionCell bull-item-content__cell bull-item-content__description-cell",
+            )
+        ]
+        posts.append(full_post_v1)
+        full_post_v2 = [
+            div
+            for div in soup.find_all(
+                "div",
+                class_="bulletinBlock bull-item-content"
+            )
+        ]
+        posts.append(full_post_v2)
+        full_post_v3 = [
+            div
+            for div in soup.find_all(
+                "div", class_="bull-item bull-item_block bull-item_block-js"
+            )
+        ]
+        posts.append(full_post_v3)
 
-        full_post_v2 = [div for div in soup.find_all("div",
-                                                     class_="bulletinBlock bull-item-content")]
-        full_post = full_post_v1 if full_post_v1 else full_post_v2
+        full_post_v4 = [
+            div for div in soup.find_all(
+                "tr",
+                class_="bull-list-item-js -exact"
+            )
+        ]
+        posts.append(full_post_v4)
 
-        for post in full_post:
-            #post_id.append(post.find('a')['name'] if post.find('a')['name']
+        full_post = [sublist for sublist in posts if len(sublist) > 0]
+
+        for post in full_post[0]:
+            # post_id.append(post.find('a')['name'] if post.find('a')['name']
             # else post.parent.a['name'])
-            if post.find('a')['name']:
-                post_id.append(post.find('a')['name'])
+            if post.find("a")["name"]:
+                post_id.append(post.find("a")["name"])
             else:
-                post.parent.a['name']
+                post_id.append(post.parent.a["name"])
 
-            if post.find('div',
-                         class_='bull-item-content__price-info-container').text:
-                is_check.append('True')
+            if post.find("div", class_="price-block__price"):
+                is_check.append("True")
             else:
-                is_check.append('False')
+                is_check.append("False")
 
-            if post.find('span',  class_='views nano-eye-text'):
+            if post.find("span", class_="views nano-eye-text"):
                 views.append(
-                    post.find('span',  class_='views nano-eye-text').text)
+                    post.find("span", class_="views nano-eye-text").text
+                )
             else:
-                views.append('0')
+                views.append("0")
+        # views = [div for div in soup.find_all("span", class_="views
+        # nano-eye-text")]
 
-        profile_links = [a["href"] for a in soup.find_all("a",
-                                                          class_="bulletinLink bull-item__self-link auto-shy")]
-        name_announcement = [a.text for a in soup.find_all("a",
-                                                           class_="bulletinLink bull-item__self-link auto-shy")]
+        profile_links = [
+            a["href"]
+            for a in soup.find_all(
+                "a", class_="bulletinLink bull-item__self-link auto-shy"
+            )
+        ]
+        name_announcement = [
+            a.text
+            for a in soup.find_all(
+                "a", class_="bulletinLink bull-item__self-link auto-shy"
+            )
+        ]
         for value in name_announcement:
-            room.append(value[0] if value[0].isdigit() or value.startswith(
-                'Гостинка') or value.startswith('Комната') else 0)
+            room.append(
+                value[0]
+                if value[0].isdigit()
+                or value.startswith("Гостинка")
+                or value.startswith("Комната")
+                else 0
+            )
 
-        cost = [div.next for div in soup.find_all("div",
-                                                     class_="price-block__price")]
-        cost = [value.replace(' ', '') for value in cost]
+        cost = [div.next for div in soup.find_all(
+            "div", class_="price-block__price")]
+        cost = [value.replace(" ", "") for value in cost]
 
-        district = [div.text for div in soup.find_all("div",
-                                                      class_="bull-item__annotation-row")]
+        district = [
+            div.text for div in soup.find_all(
+                "div", class_="bull-item__annotation-row")
+        ]
         for value in district:
-            if value.split(',')[0] == '64':
-                area.append('64,' + value.split(',')[1])
-                author.append(value.split(',')[2])
+            if value.split(",")[0] == "64":
+                area.append("64," + value.split(",")[1])
+                author.append(value.split(",")[2])
             else:
-                area.append(value.split(',')[0])
-                author.append(value.split(',')[1])
-            square.append(value.split(',')[-2] + ',' + value.split(',')[-1][
-                0] if len(value.split(',')) > 2 else 0)
+                area.append(value.split(",")[0])
+                author.append(value.split(",")[1])
+            square.append(
+                value.split(",")[-2] + "," + value.split(",")[-1][0]
+                if len(value.split(",")) > 2
+                else 0
+            )
 
-        df = pd.DataFrame({
-            'Id': post_id,
-            'Название': name_announcement,
-            'Район': 'None',
-            'Ссылка': profile_links,
-            'Просмотры': views,
-            'Стоимость': 'None',
-            'Комнат': room,
-            'Формат': is_check,
-            'Площадь': 'None',
-            'Автор': 'None',
-
-
-        })
-        for i, row in enumerate(np.where(df['Формат'] == 'True')[0].tolist()):
-            df.loc[row, 'Стоимость'] = cost[i]
-            df.loc[row, 'Район'] = area[i]
-            df.loc[row, 'Площадь'] = square[i]
-            df.loc[row, 'Автор'] = author[i]
+        df = pd.DataFrame(
+            {
+                "Id": post_id,
+                "Название": name_announcement,
+                "Район": "None",
+                "Ссылка": profile_links,
+                "Просмотры": views,
+                "Стоимость": "None",
+                "Комнат": room,
+                "Формат": is_check,
+                "Площадь": "None",
+                "Автор": "None",
+            }
+        )
+        for i, row in enumerate(np.where(df["Формат"] == "True")[0].tolist()):
+            df.loc[row, "Стоимость"] = cost[i]
+            df.loc[row, "Район"] = area[i]
+            df.loc[row, "Площадь"] = square[i]
+            df.loc[row, "Автор"] = author[i]
 
         write_profiles_to_csv(df)
         df = df[0:0]
@@ -216,7 +260,8 @@ def scrape_all_profiles(start_url):
         if page % 50 == 0:
             driver.quit()
         current_url = (
-            f'https://www.farpost.ru/vladivostok/realty/sell_flats?page={page}')
+            f"https://www.farpost.ru/vladivostok/realty/sell_flats?page={page}"
+        )
 
         time.sleep(random.uniform(2, 7))
     driver.switch_to.window(driver.window_handles[0])
@@ -225,4 +270,5 @@ def scrape_all_profiles(start_url):
 
 
 all_profiles = scrape_all_profiles(
-    "https://www.farpost.ru/vladivostok/realty/sell_flats/")
+    "https://www.farpost.ru/vladivostok/realty/sell_flats/"
+)
---
+++ user_agents.py
@@ -0,0 +1,14 @@
+USER_AGENTS = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 9; SM-T385) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 9; Redmi Note 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.127 Mobile Safari/537.36',
+        'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1',
+        'Mozilla/5.0 (Linux; Android 10; GM1910) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 10; MI 8 Lite) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 9; FLA-LX1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36 OPR/61.2.3076.56749',
+        'Mozilla/5.0 (Linux; arm_64; Android 7.1.1; Lenovo TB-8504X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 YaBrowser/20.6.1.73.01 Safari/537.36',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/86.0.4240.93 Mobile/15E148 Safari/604.1',
+        'Mozilla/5.0 (Linux; Android 8.1.0; 16th) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.81 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 9) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 DuckDuckGo/5',
+    ]
