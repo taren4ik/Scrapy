91a3211e346f0d5ad737f9cbf3b4a34c9e2601d3
Dmitry Permyakov <taren4ik@mail.ru>
2025-02-20 00:11:22 +1000

    Добавлен роут на создание таблицы

--- api_farpost/src/main.py
+++ api_farpost/src/main.py
@@ -1,11 +1,11 @@
 import os
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel, ann
 from dotenv import load_dotenv
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import delete, func, insert, select, update
 
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
@@ -21,12 +21,27 @@ engine = create_async_engine((f'postgresql+asyncpg://{DB_USER}:{DB_PASS}'
                 f'/{DB_NAME}'))
 
 
-
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
-async  def get_session():
+
+async def get_session():
+    """Generator session."""
     async with new_session() as session:
         yield session
+
+# SessionDep = Annotated[AsyncSession, Depends(get_session())]
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class BookModel(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    author: Mapped[str]
 
 
 
@@ -38,11 +53,20 @@ class Param(BaseModel):
     square: float | None = None
 
 
+@app.post("/setup_database/", summary="Создание БД",
+          tags=["Создание таблицы в БД Postgres"])
+async def setup_database():
+    async with engine.begin() as conn:
+        #await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    return {"Create Table": "Succsess"}
+
+
+
 @app.post("/sale_flat/")
 async def create_item(data: Param):
     room = data.room
     square = data.square
-
 
     return {"Выгрузка для ": f"{room}- комнатных квартир, площадью -"
                              f" {square}"}
